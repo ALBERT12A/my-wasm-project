afbf8f8f1f5459267cbada78ab3b37b470c7dad5
ALBERT12A <albert25ronald@gmail.com>
2025-08-23 10:58:41 +0330

    index.ts

---
+++ assembly/index.ts
@@ -0,0 +1,10 @@
+// assembly/index.ts
+// Sample functions for testing WebAssembly with AssemblyScript
+
+export function add(a: i32, b: i32): i32 {
+  return a + b;
+}
+
+export function multiply(a: i32, b: i32): i32 {
+  return a * b;
+}
